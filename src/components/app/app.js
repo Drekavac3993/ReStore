f2c452672a953cfb0680645fbbe3e967f934cef2
Drekavac <diesel39931@gmail.com>
2019-05-08 00:48:30 +0600

    added frame for app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { withBookstoreService } from '../hoc';
 import './app.css';
 
-const App = () => {
+const App = ({ bookStoreService }) => {
+  console.log(bookStoreService.getBooks());
   return <div>App</div>
 };
 
-export default App
+export default withBookstoreService()(App);
