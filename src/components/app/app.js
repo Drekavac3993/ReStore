9bd4c57f6a7813ea9ec68274c04302663fbbfc4d
Drekavac <diesel39931@gmail.com>
2019-05-09 00:01:35 +0600

    added style for some components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CartPage } from "../pages";
+import ShopHeader from '../shop-header';
 import './app.css';
 
 const App = () => {
   return(
-      <Switch>
-        <Route path='/' component={ HomePage } exact />
-        <Route path='/cart' component={ CartPage } />
-      </Switch>
+      <main role="main" className="container">
+          <ShopHeader numItems={ 5 } total={ 210 }/>
+          <Switch>
+            <Route path='/' component={ HomePage } exact />
+            <Route path='/cart' component={ CartPage } />
+          </Switch>
+      </main>
   )
 };
 
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -4,13 +4,17 @@ export default class BookstoreService {
             {
                 id: 1,
                 title: 'Production-Ready Microservices',
-                author: 'Susan J. Fowler'
+                author: 'Susan J. Fowler',
+                price: 32,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51oxXEG6TRL._SX379_BO1,204,203,200_.jpg'
             },
 
             {
                 id: 2,
                 title: 'Release It!',
-                author: 'Michael T. Nygard'
+                author: 'Michael T. Nygard',
+                price: 45,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'
             }
         ];
     }
